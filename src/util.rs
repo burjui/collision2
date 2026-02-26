2c80b66437fa6e18ed650b1a0ba6a4b2f2d6bfd6
Artem Borisovskiy <bytefu@gmail.com>
2026-02-26 22:42:47 +0200

    Optimal dispatch_dimensions()

--- src/util.rs
+++ src/util.rs
@@ -18,11 +18,7 @@ impl AABB {
 
 pub fn dispatch_dimensions(object_count: usize, workgroup_size: u32) -> (u32, u32, u32) {
     const MAX_DIMENSION: u32 = 65535;
-    let total_workgroups = u32::try_from(object_count)
-        .ok()
-        .and_then(|c| c.checked_add(workgroup_size - 1))
-        .map(|c| c / workgroup_size)
-        .expect("object_count too large or overflow");
+    let total_workgroups = u32::try_from(object_count).map(|c| c.div_ceil(workgroup_size)).unwrap();
     let x = total_workgroups.min(MAX_DIMENSION);
     let y = (total_workgroups.div_ceil(x)).min(MAX_DIMENSION);
     let z = total_workgroups.div_ceil(x * y);
